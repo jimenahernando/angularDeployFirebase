98d9d277e946aa36f2db9bc90b0ac533312983a5
jimena <jimenahernando@hotmail.com>
2021-09-02 22:22:05 +0200

    Pintando lista de casas

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Property } from 'src/app/interfaces/property.interface';
+import { PropertiesService } from 'src/app/services/properties.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  arrProperties: Property[];
+
+  constructor(private propertiesService: PropertiesService) {
+    this.arrProperties= [];
+  }
 
   ngOnInit(): void {
+    this.arrProperties = this.propertiesService.getAll(); 
   }
 
 }
