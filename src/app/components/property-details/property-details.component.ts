e6a2959f06fc899e38870a27b9cdad787a508220
jimena <jimenahernando@hotmail.com>
2021-09-02 22:24:06 +0200

    Detalle de cada card

--- src/app/components/property-details/property-details.component.ts
+++ src/app/components/property-details/property-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Property } from 'src/app/interfaces/property.interface';
+import { PropertiesService } from 'src/app/services/properties.service';
 
 @Component({
   selector: 'app-property-details',
@@ -7,9 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PropertyDetailsComponent implements OnInit {
 
-  constructor() { }
+  property: Property | undefined;
+
+  constructor( 
+    private activatedRoute: ActivatedRoute,
+    private propertiesService:PropertiesService) { }
 
   ngOnInit(): void {
+    this.activatedRoute.params?.subscribe(params => {
+      const id= Number(params.idProperty);
+      this.property = this.propertiesService.getById(id);
+    })
   }
 
 }
